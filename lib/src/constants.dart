6cd2b94f336c3e4cbe92cf5907a41872702573fd
mencraft <ekemboy@gmail.com>
2025-07-17 11:40:32 +0100

    Update: Bump Longswipe widget script URI to version 1.1.22 in constants.dart

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,2 +1,2 @@
 /// Default URI for the Longswipe widget script
-const String DEFAULT_URI = 'https://cdn.jsdelivr.net/npm/@longswipe/longswipe-widget@1.1.21/dist/widget.js';
+const String DEFAULT_URI = 'https://cdn.jsdelivr.net/npm/@longswipe/longswipe-widget@1.1.22/dist/widget.js';
